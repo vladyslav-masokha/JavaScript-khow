031b2906dfbe02b45758ad4030c8818151c27c55
Vladyslav <animew1243@gmail.com>
2022-06-26 20:35:14 +0300

    New design

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-'use strict';
-// TODO: Navigation
-$('.close').click(function () {
-	$('.nav').addClass('un-active-navbar');
-});
+// Navigation
+const navEl = document.getElementsByTagName('nav');
+const bodyEl = document.querySelector('body');
 
-$('.check-burger').click(function () {
-	$('.nav').removeClass('un-active-navbar');
-});
+document.getElementById('close-menu').onclick = () => {
+	navEl[0].classList.add('un-active-navbar');
+	navEl[0].style.display = 'none';
+};
+document.getElementById('check-burger').onclick = () => {
+	navEl[0].classList.remove('un-active-navbar');
+	navEl[0].style.display = 'flex';
+};
+
+// Clock
+function myClock() {
+	setTimeout(function () {
+		const d = new Date();
+		const n = d.toLocaleTimeString();
+		document.getElementById('clock').innerHTML = n;
+		myClock();
+	}, 1000);
+}
+myClock();
